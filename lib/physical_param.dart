4a93ac55a2b41215001e5f36e2498abe534dc84b
ProfAndroid <sukhanov.licey@gmail.com>
2025-10-27 21:49:43 +0300

    Добавил все файлы в экспорт

--- lib/physical_param.dart
+++ lib/physical_param.dart
@@ -4,5 +4,7 @@
 library;
 
 export 'src/physical_param_base.dart';
+export 'src/physical_param_types.dart';
+export 'src/physical_conversion_constants.dart';
 
 // TODO: Export any libraries intended for clients of this package.
