9e6f1e73e9ccf404ae840e4e6577cbc85b1b79f3
Andrey Sukhanov <sukhanov.licey@gmail.com>
2025-10-28 11:14:43 +0300

    Добавлены или изменены типы для корректного тестирования

--- lib/src/physical_param_types.dart
+++ lib/src/physical_param_types.dart
@@ -7,37 +7,37 @@ enum PhysicalType {
   dimensionless,
 }
 
-enum LengthUnits { meter, foot, kilometer, miles }
+enum LengthUnits { meter, foot, kilometer, seamiles, millimeter }
 
-enum SquareUnits { squareMeter, squareFoot, squareKilometer, squareSeamiles }
+enum SquareUnits { squareMeter, squareFoot, squareKilometer, squareSeamile }
 
-enum VelocityUnits { mps, kmph, smph, knots }
+enum VelocityUnits { mps, kmph, smph, knots, parsec }
 
 enum AccelerationUnits { mps2, kmph2, mph2 }
 
-enum TimeUnits { sec, min, hour }
+enum TimeUnits { sec, min, hour, day }
 
 Map<PhysicalType, Map<Enum, String>> physicUnitsMap = {
   PhysicalType.length: lengthUnitsMap,
   PhysicalType.velocity: velocityUnitsMap,
   PhysicalType.time: timeUnitsMap,
+  PhysicalType.acceleration: accelerationUnitsMap,
   PhysicalType.dimensionless: {},
 };
 
-Map<PhysicalType, dynamic> defaultunit = {
+Map<PhysicalType, Enum> defaultUnits = {
   PhysicalType.length: LengthUnits.meter,
   PhysicalType.time: TimeUnits.sec,
-  PhysicalType.velocity: VelocityUnits.mps,
+  // PhysicalType.velocity: VelocityUnits.mps,
   PhysicalType.acceleration: AccelerationUnits.mps2,
-  PhysicalType.square: SquareUnits.squareMeter,
-  PhysicalType.dimensionless: null,
+  // PhysicalType.square: SquareUnits.squareMeter,
 };
 
 Map<LengthUnits, String> lengthUnitsMap = {
   LengthUnits.meter: 'м',
   LengthUnits.foot: 'фут',
   LengthUnits.kilometer: 'км',
-  LengthUnits.miles: 'морские мили',
+  // LengthUnits.seamiles: 'морские мили',
 };
 
 Map<VelocityUnits, String> velocityUnitsMap = {
@@ -51,4 +51,10 @@ Map<TimeUnits, String> timeUnitsMap = {
   TimeUnits.sec: 'сек',
   TimeUnits.min: 'мин',
   TimeUnits.hour: 'час',
+};
+
+Map<AccelerationUnits, String> accelerationUnitsMap = {
+  // AccelerationUnits.mps2: 'м/с²',
+  AccelerationUnits.kmph2: 'км/ч²',
+  AccelerationUnits.mph2: 'морские_мили/ч²',
 };
